c443d67e9c5d63308980d26cc4ef7deeaad4c1c4
DerekG29 <derekgrubbs29@gmail.com>
2024-08-06 19:39:21 -0400

    updated packages

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const db = require('../../data/dbConfig');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
 
 router.post('/register', (req, res) => {
   res.end('implement register, please!');
