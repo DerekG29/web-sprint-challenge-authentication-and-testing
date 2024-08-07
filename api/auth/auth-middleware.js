bb8bba10241cff7d434e9ca658d0f82094cfb06e
DerekG29 <derekgrubbs29@gmail.com>
2024-08-06 20:48:26 -0400

    endpoints and middleware built

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,14 @@
+const Users = require('./auth-model');
+
+module.exports= {
+  validate,
+}
+
+function validate(req, res, next) {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(422).json('username and password required');
+  } else {
+    next();
+  }
+}
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,62 +1,51 @@
 const router = require('express').Router();
-const db = require('../../data/dbConfig');
+const Users = require('./auth-model');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken'); 
+const jwt = require('jsonwebtoken');
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+const { validate } = require('./auth-middleware');
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
-
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+router.post('/register', validate, async (req, res, next) => {
+ try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username });
+    if (user) {
+      res.status(422).json('username taken')
+    } else {
+      const hash = bcrypt.hashSync(password, 8);
+      const result = await Users.insert({ username, password: hash });
+      res.status(201).json(result);
+    }
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+router.post('/login', validate, async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = buildToken(user);
+      res.status(200).json({ message: `welcome, ${username}`, token });
+    } else {
+      res.status(422).json('invalid credentials')
+    }
+  } catch (error) {
+    next(error)
+  }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  const secret = process.env.JWT_SECRET || 'secret';
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
